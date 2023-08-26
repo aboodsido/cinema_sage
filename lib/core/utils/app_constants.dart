1f389e05dcc8bcb5cb6080847cca64c5a4e943d9
Abdullah Abo-Sido <odod41907@gmail.com>
2023-08-26 14:51:36 +0300

    create movie entity

---
+++ lib/core/utils/app_constants.dart
@@ -0,0 +1,4 @@
+class AppConstants {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String apiKey = 'f887323dc404a3ac7318b4fc308bb099';
+}
---
+++ lib/movies/domain/entities/movie_entity.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+class MovieEntity extends Equatable {
+  final int id;
+  final String backdropPath;
+  final String title;
+  final String overview;
+  final double voteAverage;
+  final int voteCount;
+  final List<int> genreIds;
+
+  const MovieEntity({
+    required this.id,
+    required this.backdropPath,
+    required this.title,
+    required this.overview,
+    required this.voteAverage,
+    required this.voteCount,
+    required this.genreIds,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        backdropPath,
+        title,
+        overview,
+        voteAverage,
+        voteCount,
+        genreIds,
+      ];
+}
